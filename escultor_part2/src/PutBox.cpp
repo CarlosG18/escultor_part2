5b8d417557743cebea878d556e358b20fde86231
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-11-10 14:36:03 -0300

    Update PutBox.cpp

--- escultor_part2/src/PutBox.cpp
+++ escultor_part2/src/PutBox.cpp
@@ -7,7 +7,7 @@ PutBox::~PutBox()
     //dtor
 }
 
-PutBox::PutBox(int x0, int x1, int y0, int y1, int z0, int z1)
+PutBox::PutBox(int x0, int x1, int y0, int y1, int z0, int z1,float r, float g, float b, float a)
 {
     this->x0 = x0;
     this->x1 = x1;
@@ -15,6 +15,10 @@ PutBox::PutBox(int x0, int x1, int y0, int y1, int z0, int z1)
     this->y1 = y1;
     this->z0 = z0;
     this->z1 = z1;
+    this->r = r;
+    this->g = g;
+    this->b = b;
+    this->a = a;
 }
 
 void PutBox::draw(Sculptor& s)
@@ -24,7 +28,7 @@ void PutBox::draw(Sculptor& s)
     for(k=z0; k<=z1; k++){
         for(j=y0; j<=y1; j++){
             for(i=x0; i<=x1; i++){
-                s->putVoxel(i,j,k);
+                s.putVoxel(i,j,k);
             }
         }
     }
