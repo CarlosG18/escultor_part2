fcb661a33ac6ad03641886f52491f6946459da0e
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-11-10 14:34:35 -0300

    Update main.cpp

--- escultor_part2/src/main.cpp
+++ escultor_part2/src/main.cpp
@@ -2,25 +2,21 @@
 #include <fstream>
 #include <string>
 #include <vector>
-//#include "Sculptor.h"
-//#include "PutVoxel.h"
-//#include "PutBox.h"
-//#include "PutSphere.h"
-//#include "PutEllipsoid.h"
-//#include "CutVoxel.h"
-//#include "CutBox.h"
-//#include "CutSphere.h"
-//#include "CutEllipsoid.h"
-
+#include "Sculptor.h"
+#include "PutVoxel.h"
+#include "PutBox.h"
+#include "PutSphere.h"
+#include "PutEllipsoid.h"
+#include "CutVoxel.h"
+#include "CutBox.h"
+#include "CutSphere.h"
+#include "CutEllipsoid.h"
+#include "FiguraGeometrica.h"
 
 int main(){
-
 	std::string s;
 	std::ifstream fin;
-	//std::vector<FigurasGeometricas> figs;
-	//Sculptor *sp;
-	std::vector<int> numeros;
-
+	std::vector<FigurasGeometricas*> figs;
 	float r,g,b,a;
 
 	fin.open("./file_config/infos.txt");
@@ -34,64 +30,51 @@ int main(){
 		if(s.compare("dim") == 0){
 			int larg, alt, prof;
 			fin >> larg; fin >> alt; fin >> prof;
-			//sp = new Sculptor(larg,alt,prof);
-			std::cout << "dim " << larg << " " << alt << " " << prof << "\n";
+			Sculptor s(larg,alt,prof);
 		}
 		if(s.compare("putvoxel") == 0){
 			int x0, y0, z0;
 			fin >> x0; fin >> y0; fin >> z0; 
 			fin >> r; fin >> g; fin >> b; fin >> a;
-			//sp.setColor(r,g,b);
-			//figs.push_back(new PutVoxel(x0,y0,z0));
-			std::cout << "putvoxel(" << x0 << "," << y0 << "," << z0 << "," << r << "," << g << "," << b << "," << a << ")\n";
+			figs.push_back(new PutVoxel(x0,y0,z0,r,g,b,a));
 		}
 		if(s.compare("cutvoxel") == 0){
 			int x0, y0, z0;
 			fin >> x0; fin >> y0; fin >> z0; 
-			//figs.push_back(new cutVoxel(x0,y0,z0));
-			std::cout << "cutvoxel(" << x0 << "," << y0 << "," << z0 << ")\n";
+			figs.push_back(new cutVoxel(x0,y0,z0));
 		}
 		if(s.compare("putbox") == 0){
 			int x0, x1, y0, y1, z0, z1; 
 			fin >> x0; fin >> x1; fin >> y0; fin >> y1; fin >> z0; fin >> z1; 
 			fin >> r; fin >> g; fin >> b; fin >> a;
-			//sp.setColor(r,g,b);
-			//figs.push_back(new PutBox(x0,x1,y0,y1,z0,z1));
-			std::cout << "putbox(" << x0 << "," << x1 << "," << y0 << "," << y1 << "," << z0 << "," << z1 << "," << r << "," << g << "," << b << "," << a << ")\n";
+			figs.push_back(new PutBox(x0,x1,y0,y1,z0,z1,r,g,b,a));
 		}
 		if(s.compare("cutbox") == 0){
 			int x0, x1, y0, y1, z0, z1; 
 			fin >> x0; fin >> x1; fin >> y0; fin >> y1; fin >> z0; fin >> z1;
-			//figs.push_back(new CutBox(x0,x1,y0,y1,z0,z1)); 
-			std::cout << "cutbox(" << x0 << "," << x1 << "," << y0 << "," << y1 << "," << z0 << "," << z1 << ")\n";
+			figs.push_back(new CutBox(x0,x1,y0,y1,z0,z1)); 
 		}
 		if(s.compare("putsphere") == 0){
 			int x0, y0, z0, raio; 
 			fin >> x0; fin >> y0; fin >> z0; fin >> raio; 
 			fin >> r; fin >> g; fin >> b; fin >> a;
-			//sp.setColor(r,g,b);
-			//figs.push_back(new PutSphere(x0,y0,z0,raio));
-			std::cout << "putsphere(" << x0 << "," << y0 << "," << z0 << "," << raio << "," << r << "," << g << "," << b << "," << a << ")\n";
+			figs.push_back(new PutSphere(x0,y0,z0,raio,r,g,b,a));
 		}
 		if(s.compare("cutsphere") == 0){
 			int x0, y0, z0, raio; 
 			fin >> x0; fin >> y0; fin >> z0; fin >> raio; 
-			//figs.push_back(new CutSphere(x0,y0,z0,raio));
-			std::cout << "cutsphere(" << x0 << "," << y0 << "," << z0 << "," << raio << ")\n";
+			figs.push_back(new CutSphere(x0,y0,z0,raio));
 		}
 		if(s.compare("putellipsoid") == 0){
 			int x0, y0, z0, raio; 
 			fin >> x0; fin >> y0; fin >> z0; fin >> raio; 
 			fin >> r; fin >> g; fin >> b; fin >> a;
-			//sp.setColor(r,g,b);
-			//figs.push_back(new PutEllipsoid(x0,y0,z0,raio));
-			std::cout << "putellipsoid(" << x0 << "," << y0 << "," << z0 << "," << raio << "," << r << "," << g << "," << b << "," << a << ")\n";
+			figs.push_back(new PutEllipsoid(x0,y0,z0,raio,r,g,b,a));
 		}
 		if(s.compare("cutellipsoid") == 0){
 			int x0, y0, z0, raio; 
 			fin >> x0; fin >> y0; fin >> z0; fin >> raio; 
-			//figs.push_back(new PutEllipsoid(x0,y0,z0,raio));
-			std::cout << "cutellipsoid(" << x0 << "," << y0 << "," << z0 << "," << raio << ")\n";
+			figs.push_back(new PutEllipsoid(x0,y0,z0,raio,r,g,b,a));
 		}
 	}
 
