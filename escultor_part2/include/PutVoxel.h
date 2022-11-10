907638a71b2c0e05e926c038b802d30213d3901b
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-11-10 14:18:22 -0300

    Update PutVoxel.h

--- escultor_part2/include/PutVoxel.h
+++ escultor_part2/include/PutVoxel.h
@@ -1,12 +1,13 @@
 #ifndef PUTVOXEL_H
 #define PUTVOXEL_H
-#include <FiguraGeometrica.h>
+#include "FiguraGeometrica.h"
 #include "Sculptor.h"
 
 class PutVoxel : public FiguraGeometrica
 {
     private:
         int x, y, z;
+        float r, g, b, a;
     public:
         PutVoxel(int x, int y, int z);
         virtual ~PutVoxel();
