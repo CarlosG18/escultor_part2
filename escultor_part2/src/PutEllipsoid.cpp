0011b35349f15efcb79001460a66378e193f01ef
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-11-11 08:28:58 -0300

    Update PutEllipsoid.cpp

--- escultor_part2/src/PutEllipsoid.cpp
+++ escultor_part2/src/PutEllipsoid.cpp
@@ -1,11 +1,12 @@
 #include "PutEllipsoid.h"
+#include "sculptor.h"
 
 PutEllipsoid::~PutEllipsoid()
 {
     //dtor
 }
 
-PutEllipsoid::PutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz)
+PutEllipsoid::PutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz, float r, float g, float b, float a)
 {
     this->xcenter = xcenter;
     this->ycenter = ycenter;
@@ -13,20 +14,25 @@ PutEllipsoid::PutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry
     this->rx = rx;
     this->ry = ry;
     this->rz = rz;
+    this->r = r;
+    this->g = g;
+    this->b = b;
+    this->a = a;
 }
 
 void PutEllipsoid::draw(Sculptor& s)
 {
     int i,j,k;
 
-    for(k=0; k<nz; k++){
-        for(j=0; j<ny; j++){
-            for(i=0; i<nx; i++){
+    s.setColor(r,g,b,a);
+
+    for(k=0; k<s.get_nz(); k++){
+        for(j=0; j<s.get_ny(); j++){
+            for(i=0; i<s.get_nx(); i++){
                 if(((((float(i-xcenter)/rx))*(float(i-xcenter)/rx))+((float(j-ycenter)/ry)*(float(j-ycenter)/ry))+(((float(k-zcenter)/rz))*((float(k-zcenter)/rz)))) <= 1.0 ){
-                    s->putVoxel(i,j,k);
+                    s.putVoxel(i,j,k);
                 }
             }
         }
     }
 }
-
