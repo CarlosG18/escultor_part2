9ae27fd063781c9df3f899da4abb48cef00e83cd
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-11-11 08:25:20 -0300

    Update PutSphere.h

--- escultor_part2/include/PutSphere.h
+++ escultor_part2/include/PutSphere.h
@@ -8,8 +8,9 @@ class PutSphere : public FiguraGeometrica
 {
     private:
         int xcenter, ycenter, zcenter, radius;
+        float r,g,b,a;
     public:
-        PutSphere(int xcenter, int ycenter, int zcenter, int radius);
+        PutSphere(int xcenter, int ycenter, int zcenter, int radius, float r, float g, float b, float a);
         virtual ~PutSphere();
         void draw(Sculptor &s);
 };
