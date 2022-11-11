d54bde980d23d80925e4d342a74c64505505e8ef
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-11-11 08:29:25 -0300

    Update PutSphere.cpp

--- escultor_part2/src/PutSphere.cpp
+++ escultor_part2/src/PutSphere.cpp
@@ -1,27 +1,34 @@
 #include "PutSphere.h"
+#include "sculptor.h"
 
 PutSphere::~PutSphere()
 {
     //dtor
 }
 
-PutSphere::PutSphere(int xcenter, int ycenter, int zcenter, int radius)
+PutSphere::PutSphere(int xcenter, int ycenter, int zcenter, int radius, float r, float g, float b, float a)
 {
     this->xcenter = xcenter;
     this->ycenter = ycenter;
     this->zcenter = zcenter;
     this->radius = radius;
+    this->r = r;
+    this->g = g;
+    this->b = b;
+    this->a = a;
 }
 
 void PutSphere::draw(Sculptor& s)
 {
     int i,j,k;
 
-    for(k=0; k<nz; k++){
-        for(j=0; j<ny; j++){
-            for(i=0; i<nx; i++){
+    s.setColor(r,g,b,a);
+
+    for(k=0; k<s.get_nz(); k++){
+        for(j=0; j<s.get_ny(); j++){
+            for(i=0; i<s.get_nx(); i++){
                 if((((i-xcenter)*(i-xcenter))+((j-ycenter)*(j-ycenter))+((k-zcenter)*(k-zcenter))) <= ((radius)*(radius))){
-                    s->putVoxel(i,j,k);
+                    s.putVoxel(i,j,k);
                 }
             }
         }
