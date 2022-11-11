b15e284f50c2e42ac77dccdc356f76b997dcee09
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2022-11-11 08:29:55 -0300

    Update PutVoxel.cpp

--- escultor_part2/src/PutVoxel.cpp
+++ escultor_part2/src/PutVoxel.cpp
@@ -1,23 +1,25 @@
 #include "PutVoxel.h"
 #include "Sculptor.h"
+#include "figurageometrica.h"
 
 PutVoxel::~PutVoxel()
 {
     //dtor
 }
 
-PutVoxel::PutVoxel(int x, int y, int z)
+PutVoxel::PutVoxel(int x, int y, int z, float r, float g, float b, float a)
 {
     this->x = x;
     this->y = y;
     this->z = z;
+    this->r = r;
+    this->g = g;
+    this->b = b;
+    this->a= a;
 }
 
 void PutVoxel::draw(Sculptor& s)
 {
-    s->v[z][y][x].isOn = true;
-    s->v[z][y][x].r = r;
-    s->v[z][y][x].g = g;
-    s->v[z][y][x].b = b;
-    s->v[z][y][x].a = a;
+    s.setColor(r,g,b,a);
+    s.putVoxel(x,y,z);
 }
